537676dd1abc279c42c2ae702d3b025d49bba1ce
ikcii <ikcikor@gmail.com>
2025-10-20 01:38:08 +0200

    more stylix and make it extra dark

--- users/accounts/ikci/home.nix
+++ users/accounts/ikci/home.nix
@@ -135,15 +135,22 @@
 
 	stylix = {
 		enable = true;
+		autoEnable = true;
 		image = ./wallpaper.png;
+		polarity = "dark";
 		opacity = {
 			applications = 0.8;
 			popups = 0.8;
 			desktop = 0.8;
 			terminal = 0.8;
 		};
-		autoEnable = true;
 
 		fonts.monospace.name = "JetBrainsMono Nerd Font";
+
+		targets = {
+			sway.enable = true;
+			swaylock.enable = true;
+			vesktop.enable = true;
+		};
 	};
 }
