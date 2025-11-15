3691e5394702ebf8efad817ebf1a2cc6a6264f3f
ikcii <ikcikor@gmail.com>
2025-11-15 20:34:37 +0100

    add some nvim settings from kickstart-nvim

--- users/ikci/nvim/init.lua
+++ users/ikci/nvim/init.lua
@@ -1,5 +1,60 @@
-vim.g.mapleader = " "
+-- Huge thanks to nvim-lua/kickstart.nvim (MIT license) and Gerg-L/mnw (MIT license)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.showmode = false
+
+vim.schedule(function()
+	vim.o.clipboard = 'unamedplus'
+end)
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.updatetime = 250
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.inccommand = 'split'
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 12
+
+vim.o.confirm = true
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- TODO: Potentially add logic for standalone mode
 require("lazy").setup({
 	dev = {
 		path = mnw.configDir .. "/pack/mnw/opt",
@@ -18,7 +73,7 @@ require("lazy").setup({
 	},
 
 	spec = {
-		{ import = "plugins" },
+		import = "plugins",
 	},
 
 	rocks = {
--- users/ikci/nvim/lua/plugins/treesitter.lua
+++ users/ikci/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,15 @@
 return {
+
 	"nvim-treesitter/nvim-treesitter",
+
 	lazy = false,
+
+	opts = {
+		auto_install = false,
+	},
+
+	-- TODO: Potentially add logic for standalone mode
+	init = function()
+    		require("nvim-treesitter.install").compilers = { "nvim" }
+  	end,
 }
