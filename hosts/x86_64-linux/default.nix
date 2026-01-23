d7461329579784e5ded4562713414b7fce9428b9
ikcii <ikcikor@gmail.com>
2026-01-23 19:01:14 +0100

    fix: disable dnssec because TLS already enabled

--- hosts/x86_64-linux/default.nix
+++ hosts/x86_64-linux/default.nix
@@ -13,9 +13,9 @@
       efiSupport = true;
       device = "nodev";
     };
-  }; 
+  };
   boot.tmp.cleanOnBoot = true;
-  
+
   # --- Kernel ---
   # Default to latest kernel
   boot.kernelPackages = lib.mkOptionDefault pkgs.linuxPackages_latest;
@@ -31,18 +31,17 @@
 
   # --- Encrypted DNS ---
   networking = {
-    nameservers = [ "127.0.0.1" "::1" ];
     networkmanager.dns = "systemd-resolved";
   };
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    dnsovertls = "true";
-    domains = [ "~." ];
-
-    settings.Resolve.DNS = [
-      "194.242.2.4#base.dns.mullvad.net"
-      "2a07:e340::4#base.dns.mullvad.net"
-    ];
+    settings.Resolve = {
+      DNS = [
+        "194.242.2.4#base.dns.mullvad.net"
+        "2a07:e340::4#base.dns.mullvad.net"
+      ];
+      DNSOverTLS = true;
+      Domains = [ "~." ];
+    };
   };
 }
