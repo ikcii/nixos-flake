e20294d11441e25618d59dee5e3355ef94510613
ikcii <ikcikor@gmail.com>
2026-02-17 22:07:50 +0100

    switch to unbound dns and whitelist Arma Reforger telemetry

--- hosts/x86_64-linux/default.nix
+++ hosts/x86_64-linux/default.nix
@@ -30,18 +30,65 @@
   services.zerotierone.enable = true;
 
   # --- Encrypted DNS ---
-  networking = {
-    networkmanager.dns = "systemd-resolved";
-  };
-  services.resolved = {
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [
+    "127.0.0.1"
+    "::1"
+  ];
+  services.unbound = {
     enable = true;
-    settings.Resolve = {
-      DNS = [
-        "194.242.2.4#base.dns.mullvad.net"
-        "2a07:e340::4#base.dns.mullvad.net"
+    settings = {
+      server = {
+        interface = [
+          "127.0.0.1"
+          "::1"
+        ];
+        access-control = [
+          "127.0.0.0/8 allow"
+          "::1/128 allow"
+        ];
+
+        hide-identity = "yes";
+        hide-version = "yes";
+      };
+
+      forward-zone = [
+        # Arma
+        {
+          name = "bohemia.net";
+          forward-addr = [
+            "194.242.2.2@853#dns.mullvad.net"
+            "2a07:e340::2@853#dns.mullvad.net"
+          ];
+          forward-tls-upstream = "yes";
+        }
+        {
+          name = "armaplatform.com";
+          forward-addr = [
+            "194.242.2.2@853#dns.mullvad.net"
+            "2a07:e340::2@853#dns.mullvad.net"
+          ];
+          forward-tls-upstream = "yes";
+        }
+        {
+          name = "bistudio.com";
+          forward-addr = [
+            "194.242.2.2@853#dns.mullvad.net"
+            "2a07:e340::2@853#dns.mullvad.net"
+          ];
+          forward-tls-upstream = "yes";
+        }
+
+        # Global
+        {
+          name = ".";
+          forward-addr = [
+            "194.242.2.4@853#base.dns.mullvad.net"
+            "2a07:e340::4@853#base.dns.mullvad.net"
+          ];
+          forward-tls-upstream = "yes";
+        }
       ];
-      DNSOverTLS = true;
-      Domains = [ "~." ];
     };
   };
 }
