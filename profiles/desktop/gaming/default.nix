4e58e62d03f3750bc14809c565aafcba979649f6
ikcii <ikcikor@gmail.com>
2026-04-28 16:37:26 +0200

    add wayvr and wivrn, remove dw-proton

--- profiles/desktop/gaming/default.nix
+++ profiles/desktop/gaming/default.nix
@@ -7,9 +7,18 @@
     enable = true;
     extraCompatPackages = [
       pkgs.proton-ge-bin
-      inputs.dw-proton.packages.${pkgs.stdenv.hostPlatform.system}.dw-proton
+      # inputs.dw-proton.packages.${pkgs.stdenv.hostPlatform.system}.dw-proton
     ];
   };
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
+  services.wivrn = {
+    enable = true;
+    openFirewall = true;
+  };
+  environment.systemPackages = with pkgs; [
+    wayvr
+    opencomposite
+    android-tools
+  ];
 }
